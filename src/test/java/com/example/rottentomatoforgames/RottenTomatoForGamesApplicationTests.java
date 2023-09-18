92f854f5860633027d6d61cc8bd0c733a2e400ea
GabrielleHandy <gehandy123@gmail.com>
2023-09-18 16:59:10 -0400

    Intialize Springboot with maven and SpringWeb and Spring Dev Tool dependencies

---
+++ src/test/java/com/example/rottentomatoforgames/RottenTomatoForGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rottentomatoforgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RottenTomatoForGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
