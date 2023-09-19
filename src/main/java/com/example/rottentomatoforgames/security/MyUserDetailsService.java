6e4d0dae7ea4c49bb430798a4554afdcc70c81c9
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 12:02:37 -0400

    Add UserService  MyUserDetailsService

--- src/main/java/com/example/rottentomatoforgames/security/MyUserDetailsService.java
+++ src/main/java/com/example/rottentomatoforgames/security/MyUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.example.rottentomatoforgames.security;
 
+import com.example.rottentomatoforgames.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,6 +9,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
-
-
+    public UserService userService;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
