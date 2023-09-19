acc6a68f248b07c31275a3b1e2f58d12826e3947
GabrielleHandy <gehandy123@gmail.com>
2023-09-18 23:15:32 -0400

    Add User model attributes

--- src/main/java/com/example/rottentomatoforgames/model/User.java
+++ src/main/java/com/example/rottentomatoforgames/model/User.java
@@ -1,11 +1,24 @@
 package com.example.rottentomatoforgames.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String userName;
+    @Column(unique = true)
+    private String emailAddress;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column
+    private String password;
+
+
 
 }
