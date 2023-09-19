94dbba93108b99f44dd6bed6094ef33c5a3213e5
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 14:22:52 -0400

    Add AuthenticationManager Bean  to  Security Configuration

--- src/main/java/com/example/rottentomatoforgames/security/SecurityConfiguration.java
+++ src/main/java/com/example/rottentomatoforgames/security/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.example.rottentomatoforgames.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,4 +33,9 @@ public class SecurityConfiguration {
         http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
 }
