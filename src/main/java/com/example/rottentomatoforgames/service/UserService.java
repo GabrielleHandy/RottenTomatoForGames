8bec279181dfb109d6eda16621c33debb65f5a4b
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 11:42:26 -0400

    Add create user function in user service

--- src/main/java/com/example/rottentomatoforgames/service/UserService.java
+++ src/main/java/com/example/rottentomatoforgames/service/UserService.java
@@ -1,5 +1,9 @@
 package com.example.rottentomatoforgames.service;
+import com.example.rottentomatoforgames.exception.InformationExistException;
 import com.example.rottentomatoforgames.model.User;
+import com.example.rottentomatoforgames.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +12,19 @@ import java.util.logging.Logger;
 @Service
 public class UserService {
     Logger logger = Logger.getLogger(UserService.class.getName());
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
-
-
-
+    @Autowired
+    public UserService(@Lazy PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+    public User createUser(User userObj){
+        if(!userRepository.existsByEmailAddress(userObj.getEmailAddress())){
+            userObj.setPassword(passwordEncoder.encode(userObj.getPassword()));
+            return userRepository.save(userObj);
+        }
+        throw new InformationExistException("User with email address " + userObj.getEmailAddress()+ " already exists.");
+    }
 }
