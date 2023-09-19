35256c708d224dc70234b2eae0a39a89a83920b3
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 15:43:31 -0400

    Create UserProfileModel

---
+++ src/main/java/com/example/rottentomatoforgames/model/UserProfileModel.java
@@ -0,0 +1,11 @@
+package com.example.rottentomatoforgames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="profiles")
+public class UserProfileModel {
+
+}
