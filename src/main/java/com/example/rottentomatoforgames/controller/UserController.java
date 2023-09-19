bb27e605ee2021bfa83ab4c0dcc9a86ab1b4c887
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 15:11:06 -0400

    Create login  User endpoint in UserController

--- src/main/java/com/example/rottentomatoforgames/controller/UserController.java
+++ src/main/java/com/example/rottentomatoforgames/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.example.rottentomatoforgames.controller;
 
+import com.example.rottentomatoforgames.model.LoginResponse;
 import com.example.rottentomatoforgames.model.User;
+import com.example.rottentomatoforgames.model.request.LoginRequest;
 import com.example.rottentomatoforgames.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("auth/users")
@@ -24,4 +29,12 @@ public class UserController {
         return userService.createUser(userObj);
     }
 
+    @PostMapping("/login/")
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest){
+        Optional<String> jwtToken = userService.loginUser(loginRequest);
+        if (jwtToken.isPresent()){
+            return ResponseEntity.ok(new LoginResponse(jwtToken.get()));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Authentication failed"));
+    }
 }
