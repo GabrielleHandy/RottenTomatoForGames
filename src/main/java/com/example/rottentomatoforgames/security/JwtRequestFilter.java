fb84f8e9eb6edf0756f385474537258ccc8629bb
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 12:56:42 -0400

    Autowired Myuserdetailsservice and JwtUtils in  JwtRequestFilter

--- src/main/java/com/example/rottentomatoforgames/security/JwtRequestFilter.java
+++ src/main/java/com/example/rottentomatoforgames/security/JwtRequestFilter.java
@@ -1,5 +1,6 @@
 package com.example.rottentomatoforgames.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -8,9 +9,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
+    Logger logger = Logger.getLogger(JwtRequestFilter.class.getName());
+    private MyUserDetailsService myUserDetailsService;
+    private JwtUtils jwtUtils;
+    @Autowired
+    public void setJwtUtils(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+    @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
