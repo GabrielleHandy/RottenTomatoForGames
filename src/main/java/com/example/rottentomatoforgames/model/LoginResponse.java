456504fa4506b66ee6aaf882084d571fda50bcaf
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 15:04:17 -0400

    Create constructor and getter and setters in Login Response

--- src/main/java/com/example/rottentomatoforgames/model/LoginResponse.java
+++ src/main/java/com/example/rottentomatoforgames/model/LoginResponse.java
@@ -1,6 +1,18 @@
 package com.example.rottentomatoforgames.model;
 
 public class LoginResponse {
+    private String jwt;
 
+    public LoginResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
 
 }
