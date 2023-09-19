78ea7e64d462aac540b3f5bca9a015b542f16216
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 12:52:10 -0400

    Create JwtRequestFilter

--- src/main/java/com/example/rottentomatoforgames/security/JwtUtils.java
+++ src/main/java/com/example/rottentomatoforgames/security/JwtUtils.java
@@ -1,6 +1,5 @@
 package com.example.rottentomatoforgames.security;
 
-import com.example.rottentomatoforgames.model.User;
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
