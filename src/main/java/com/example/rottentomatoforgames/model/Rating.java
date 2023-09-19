66464e57ef0f9898cf2fdff3c6a9ce7d90c31c15
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 17:21:59 -0400

    Add attributes to Rating Model

--- src/main/java/com/example/rottentomatoforgames/model/Rating.java
+++ src/main/java/com/example/rottentomatoforgames/model/Rating.java
@@ -1,9 +1,31 @@
 package com.example.rottentomatoforgames.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
 
 @Entity
 @Table(name= "ratings")
 public class Rating {
+
+    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JsonIgnore
+    private User ratedBy;
+
+    @ManyToOne
+    @JsonIgnore
+    private Game game;
+
+    @Column
+    private int rating;
+
+    @Column
+    private String review;
 }
