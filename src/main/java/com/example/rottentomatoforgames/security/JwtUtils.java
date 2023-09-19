def11d5093c312e88e10b0347a4706303cba1f00
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 12:49:08 -0400

    validateJwtToken function to JWT Utils

--- src/main/java/com/example/rottentomatoforgames/security/JwtUtils.java
+++ src/main/java/com/example/rottentomatoforgames/security/JwtUtils.java
@@ -1,12 +1,12 @@
 package com.example.rottentomatoforgames.security;
 
 import com.example.rottentomatoforgames.model.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Service
@@ -30,5 +30,23 @@ public class JwtUtils {
 
     public String getUserNameFromJwtToken(String token){
         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String token){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(token);
+            return true;
+        }catch(SecurityException e){
+            logger.log(Level.SEVERE, "Invalid JWT signature {}" + e.getMessage());
+        }catch(MalformedJwtException e){
+            logger.log(Level.SEVERE, "Invalid JWT token {}" + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.log(Level.SEVERE, "JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
     }
 }
