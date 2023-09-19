057fd1067031707437393d11beeb0d8375202005
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 14:58:21 -0400

    Autowire UserService in User Controller

--- src/main/java/com/example/rottentomatoforgames/controller/UserController.java
+++ src/main/java/com/example/rottentomatoforgames/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.example.rottentomatoforgames.controller;
 
+import com.example.rottentomatoforgames.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth/users")
 public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
