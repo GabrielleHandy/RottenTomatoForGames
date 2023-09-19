868f99f7a12147d45f40b39ca698e15c6c982443
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 15:56:31 -0400

    Autowire userProfileService and userService in  UserProfileService

--- src/main/java/com/example/rottentomatoforgames/service/UserProfileService.java
+++ src/main/java/com/example/rottentomatoforgames/service/UserProfileService.java
@@ -1,8 +1,16 @@
 package com.example.rottentomatoforgames.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserProfileService {
+    private final UserProfileService userProfileService;
+    private final UserService userService;
 
+    @Autowired
+    public UserProfileService(UserProfileService userProfileService, UserService userService) {
+        this.userProfileService = userProfileService;
+        this.userService = userService;
+    }
 }
