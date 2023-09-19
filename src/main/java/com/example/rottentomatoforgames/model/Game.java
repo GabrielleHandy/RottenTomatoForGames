0a6543ce81a4e967be55a1ea53e0f19a0426459d
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 17:14:06 -0400

    Create Rating Model

--- src/main/java/com/example/rottentomatoforgames/model/Game.java
+++ src/main/java/com/example/rottentomatoforgames/model/Game.java
@@ -1,6 +1,7 @@
 package com.example.rottentomatoforgames.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name= "games")
---
+++ src/main/java/com/example/rottentomatoforgames/model/Rating.java
@@ -0,0 +1,9 @@
+package com.example.rottentomatoforgames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "ratings")
+public class Rating {
+}
