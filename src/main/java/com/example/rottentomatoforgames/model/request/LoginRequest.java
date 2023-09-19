75f0333899ac5fd89e2030b581560aa2d9195831
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 14:44:26 -0400

    Add constructors to LoginRequest

--- src/main/java/com/example/rottentomatoforgames/model/request/LoginRequest.java
+++ src/main/java/com/example/rottentomatoforgames/model/request/LoginRequest.java
@@ -3,6 +3,14 @@ package com.example.rottentomatoforgames.model.request;
 public class LoginRequest {
     private String emailAddress;
     private String password;
+    public LoginRequest() {
+    }
+
+    public LoginRequest(String emailAddress, String password) {
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
 
 
 }
