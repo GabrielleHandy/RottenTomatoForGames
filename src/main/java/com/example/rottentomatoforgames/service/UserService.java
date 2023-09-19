e7fe31e712fad087f53e7e46b332885ec1ca02b2
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 11:27:39 -0400

    Add basic variables to user Service

--- src/main/java/com/example/rottentomatoforgames/service/UserService.java
+++ src/main/java/com/example/rottentomatoforgames/service/UserService.java
@@ -1,7 +1,16 @@
 package com.example.rottentomatoforgames.service;
+import com.example.rottentomatoforgames.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class UserService {
+    Logger logger = Logger.getLogger(UserService.class.getName());
+    private PasswordEncoder passwordEncoder;
+
+
+
 
 }
