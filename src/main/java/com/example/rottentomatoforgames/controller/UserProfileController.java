f963bf912b52c6962bc7f763e044b0a717f39025
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 16:56:18 -0400

    AutoWire userProfileService in UserProfileService

--- src/main/java/com/example/rottentomatoforgames/controller/UserProfileController.java
+++ src/main/java/com/example/rottentomatoforgames/controller/UserProfileController.java
@@ -1,4 +1,19 @@
 package com.example.rottentomatoforgames.controller;
 
+import com.example.rottentomatoforgames.model.UserProfile;
+import com.example.rottentomatoforgames.service.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/profiles")
 public class UserProfileController {
+    UserProfileService userProfileService;
+    @Autowired
+    public void setUserProfileService(UserProfileService userProfileService) {
+        this.userProfileService = userProfileService;
+    }
+
 }
