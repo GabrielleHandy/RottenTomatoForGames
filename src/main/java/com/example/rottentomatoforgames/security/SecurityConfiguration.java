8248a4297fdd2f7e6b3c3927d3615eeb6e086766
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 10:27:49 -0400

    Add filterChain to  Security Configuration

--- src/main/java/com/example/rottentomatoforgames/security/SecurityConfiguration.java
+++ src/main/java/com/example/rottentomatoforgames/security/SecurityConfiguration.java
@@ -3,12 +3,29 @@ package com.example.rottentomatoforgames.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();}
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/auth/users", "/auth/users/login/","/auth/users/register/" ).permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS )
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+
+        return http.build();
+    }
 }
