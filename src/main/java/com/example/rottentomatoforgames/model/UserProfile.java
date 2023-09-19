52512440fc1debdceff9bc1e4ec9a40006114763
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 16:34:41 -0400

    Add attributes getters and setters and Constructor UserProfileModel

--- src/main/java/com/example/rottentomatoforgames/model/UserProfile.java
+++ src/main/java/com/example/rottentomatoforgames/model/UserProfile.java
@@ -3,6 +3,7 @@ package com.example.rottentomatoforgames.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="profiles")
@@ -15,9 +16,23 @@ public class UserProfile {
     private Long id;
     @OneToOne
     @JsonIgnore
-    private User userId;
+    private User user;
 //    List<Ratings>
 //    List<Follower>
 
+    @Column
+    private Date lastActive;
 
+    public UserProfile() {
+        this.lastActive = new Date();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+
+    public void setLastActive() {
+        this.lastActive = new Date();
+    }
 }
