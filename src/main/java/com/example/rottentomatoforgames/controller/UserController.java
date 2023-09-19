261fcda9302f94b78fa6f225e254fbac7e551339
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 15:00:20 -0400

    Create register User endpoint in UserController

--- src/main/java/com/example/rottentomatoforgames/controller/UserController.java
+++ src/main/java/com/example/rottentomatoforgames/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.rottentomatoforgames.controller;
 
+import com.example.rottentomatoforgames.model.User;
 import com.example.rottentomatoforgames.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +16,10 @@ public class UserController {
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/register/")
+    public User createUser(@RequestBody User userObj){
+        return userService.createUser(userObj);
     }
 }
