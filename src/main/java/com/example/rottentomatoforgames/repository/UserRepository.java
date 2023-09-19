e72c327eba7817d81c28aad4314b8280b0209c13
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 10:19:33 -0400

    Create Security Configuration file

--- src/main/java/com/example/rottentomatoforgames/repository/UserRepository.java
+++ src/main/java/com/example/rottentomatoforgames/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.example.rottentomatoforgames.repository;
 
+import com.example.rottentomatoforgames.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByEmailAddress(String emailAddress);
 }
---
+++ src/main/java/com/example/rottentomatoforgames/security/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.rottentomatoforgames.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration {
+
+
+}
