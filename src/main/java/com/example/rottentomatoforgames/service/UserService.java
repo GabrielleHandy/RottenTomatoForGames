f8081ab0f14646d4ef87afbc95363354266e92f2
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 14:24:53 -0400

    Autowire Jwutils and authenticationManager in user Service

--- src/main/java/com/example/rottentomatoforgames/service/UserService.java
+++ src/main/java/com/example/rottentomatoforgames/service/UserService.java
@@ -2,8 +2,10 @@ package com.example.rottentomatoforgames.service;
 import com.example.rottentomatoforgames.exception.InformationExistException;
 import com.example.rottentomatoforgames.model.User;
 import com.example.rottentomatoforgames.repository.UserRepository;
+import com.example.rottentomatoforgames.security.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +16,14 @@ public class UserService {
     Logger logger = Logger.getLogger(UserService.class.getName());
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
-
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
     @Autowired
-    public UserService(@Lazy PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    public UserService(@Lazy PasswordEncoder passwordEncoder, UserRepository userRepository, @Lazy AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
     }
     public User findUserByEmail(String email){
         return userRepository.findUserByEmailAddress(email);
