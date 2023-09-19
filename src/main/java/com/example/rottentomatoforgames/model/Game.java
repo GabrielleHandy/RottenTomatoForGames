b8d7d0e69c4e7d9b572a5a05a7248697ff88476a
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 17:13:09 -0400

    Add attributes to  Game Model

--- src/main/java/com/example/rottentomatoforgames/model/Game.java
+++ src/main/java/com/example/rottentomatoforgames/model/Game.java
@@ -5,7 +5,26 @@ import javax.persistence.*;
 @Entity
 @Table(name= "games")
 public class Game {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column
+    private String title;
+
+    @Column
+    private int averageRating;
+
+
+    @Column
+    private List<Rating> ratings;
+
+    @Column
+    private List<Genres> genres;
+
+    @Column
+    private int releaseYear;
 
 }
 
