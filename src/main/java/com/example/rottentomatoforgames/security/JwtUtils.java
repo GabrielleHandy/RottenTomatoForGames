785037f2af19d8efbbdf2a394fc4428f26aa7399
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 12:37:22 -0400

    Add generateJWTToken function to JWT Utils

--- src/main/java/com/example/rottentomatoforgames/security/JwtUtils.java
+++ src/main/java/com/example/rottentomatoforgames/security/JwtUtils.java
@@ -1,8 +1,11 @@
 package com.example.rottentomatoforgames.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 @Service
@@ -13,7 +16,14 @@ public class JwtUtils {
     private String jwtSecret;
 
     @Value("${jwt-expiration-ms}")
-    private String jwtExpMs;
+    private int jwtExpMs;
 
-
+    public String generateJwtToken(MyUserDetails myUserDetails){
+        return Jwts.builder()
+                .setSubject(myUserDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
 }
