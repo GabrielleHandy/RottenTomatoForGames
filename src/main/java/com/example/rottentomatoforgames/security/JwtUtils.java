d00229049d642130e2d8a234c34868f1471acc2c
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 10:35:28 -0400

    Create JwtUtils file

---
+++ src/main/java/com/example/rottentomatoforgames/security/JwtUtils.java
@@ -0,0 +1,12 @@
+package com.example.rottentomatoforgames.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class JwtUtils {
+
+
+}
