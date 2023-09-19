fbd7101b033a2ccb044627bc4786d3f46327dbdc
GabrielleHandy <gehandy123@gmail.com>
2023-09-19 11:45:10 -0400

    Create MyUserDetails and implement UserDetails methods

---
+++ src/main/java/com/example/rottentomatoforgames/security/MyUserDetails.java
@@ -0,0 +1,45 @@
+package com.example.rottentomatoforgames.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class MyUserDetails implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
