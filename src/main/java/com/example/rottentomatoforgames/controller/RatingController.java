3a0744bff2acb0810fb1cf4db45bbe72712025d7
GabrielleHandy <gehandy123@gmail.com>
2023-09-20 00:47:44 -0400

    Autowire rating Service Rating Controller

--- src/main/java/com/example/rottentomatoforgames/controller/RatingController.java
+++ src/main/java/com/example/rottentomatoforgames/controller/RatingController.java
@@ -1,8 +1,14 @@
 package com.example.rottentomatoforgames.controller;
 
+import com.example.rottentomatoforgames.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RatingController {
-
+    RatingService ratingService;
+    @Autowired
+    public void setRatingService(RatingService ratingService) {
+        this.ratingService = ratingService;
+    }
 }
