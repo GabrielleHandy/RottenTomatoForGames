7c8ae3fa3b8b086168d363299ea1fd7e8fa810fa
GabrielleHandy <gehandy123@gmail.com>
2023-09-20 00:11:47 -0400

    Create Rating repository

---
+++ src/main/java/com/example/rottentomatoforgames/repository/RatingRepository.java
@@ -0,0 +1,12 @@
+package com.example.rottentomatoforgames.repository;
+
+import com.example.rottentomatoforgames.model.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+    List<Rating> findAllByGame_Id(Long gameId);
+}
