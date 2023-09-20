06a4c9abb69d4e7d0231ccda8c6b56dba7f8a29e
GabrielleHandy <gehandy123@gmail.com>
2023-09-20 01:29:51 -0400

    Create GameController

--- src/main/java/com/example/rottentomatoforgames/service/GameService.java
+++ src/main/java/com/example/rottentomatoforgames/service/GameService.java
@@ -1,8 +1,15 @@
 package com.example.rottentomatoforgames.service;
 
+import com.example.rottentomatoforgames.repository.GameRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
 
 }
