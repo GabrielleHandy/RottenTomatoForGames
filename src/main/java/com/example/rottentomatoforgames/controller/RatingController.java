f58d01d5f4644af0c25bd68c55d277b8cd0e159e
GabrielleHandy <gehandy123@gmail.com>
2023-09-20 00:51:25 -0400

    Add find all endpoint to Rating Controller

--- src/main/java/com/example/rottentomatoforgames/controller/RatingController.java
+++ src/main/java/com/example/rottentomatoforgames/controller/RatingController.java
@@ -1,14 +1,25 @@
 package com.example.rottentomatoforgames.controller;
 
+import com.example.rottentomatoforgames.model.Rating;
 import com.example.rottentomatoforgames.service.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/ratings")
 public class RatingController {
     RatingService ratingService;
     @Autowired
     public void setRatingService(RatingService ratingService) {
         this.ratingService = ratingService;
+    }
+
+    @GetMapping("/")
+    public List<Rating> getAllRatings(){
+        return ratingService.getAllRatings();
     }
 }
--- src/main/java/com/example/rottentomatoforgames/service/RatingService.java
+++ src/main/java/com/example/rottentomatoforgames/service/RatingService.java
@@ -1,7 +1,21 @@
 package com.example.rottentomatoforgames.service;
 
+import com.example.rottentomatoforgames.model.Rating;
+import com.example.rottentomatoforgames.repository.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RatingService {
+    private final RatingRepository ratingRepository;
+    @Autowired
+    public RatingService(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
+
+    public List<Rating> getAllRatings() {
+        return ratingRepository.findAll();
+    }
 }
