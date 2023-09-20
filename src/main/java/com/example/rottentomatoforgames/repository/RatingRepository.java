a1a7c06ea69347ab405d7c1fca6dc00fad8326f5
GabrielleHandy <gehandy123@gmail.com>
2023-09-20 00:38:56 -0400

    Add find rating by game id to rating repository

--- src/main/java/com/example/rottentomatoforgames/repository/RatingRepository.java
+++ src/main/java/com/example/rottentomatoforgames/repository/RatingRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
     List<Rating> findAllByGame_Id(Long gameId);
-    Rating findFirstBy
+
 }
