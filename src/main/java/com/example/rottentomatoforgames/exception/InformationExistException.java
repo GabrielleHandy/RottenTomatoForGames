039cc0614917666f8865a04c8f665eea7c8e89a5
GabrielleHandy <gehandy123@gmail.com>
2023-09-20 11:50:38 -0400

    Add Java Doc strings to Exceptions

--- src/main/java/com/example/rottentomatoforgames/exception/InformationExistException.java
+++ src/main/java/com/example/rottentomatoforgames/exception/InformationExistException.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException {
+    /**
+     * Returns a message with Http Conflict status for when Information has already been added.
+     * @param message
+     * Message sent with Http Conflict status number
+     */
     public InformationExistException(String message){
         super(message);
     }
--- src/main/java/com/example/rottentomatoforgames/exception/InformationNotFoundException.java
+++ src/main/java/com/example/rottentomatoforgames/exception/InformationNotFoundException.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class InformationNotFoundException extends RuntimeException{
+    /**
+     * Returns a message with Http Not Found status for when Information cant be found in the database.
+     * @param message
+     * Message sent with Http Not Found status number
+     */
     public InformationNotFoundException(String message) {
         super(message);
     }
