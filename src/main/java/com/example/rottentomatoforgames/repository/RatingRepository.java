d6da4ef1c2d74173f58156472b24e4088c45f61c
GabrielleHandy <gehandy123@gmail.com>
2023-09-20 04:25:04 -0400

    "Add ExistBy functions in RatingRepository "

--- src/main/java/com/example/rottentomatoforgames/repository/RatingRepository.java
+++ src/main/java/com/example/rottentomatoforgames/repository/RatingRepository.java
@@ -1,6 +1,7 @@
 package com.example.rottentomatoforgames.repository;
 
 import com.example.rottentomatoforgames.model.Rating;
+import com.example.rottentomatoforgames.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,7 @@ public interface RatingRepository extends JpaRepository<Rating, Long> {
     List<Rating> findAllByGame_Id(Long gameId);
     boolean existsByRatedBy_IdAndGame_Id(Long ratedBy_id, Long game_id);
 
+    boolean existsByRatedByAndGame_Id(UserProfile userProfile, Long gameId);
+
+    boolean existsByRatedByAndId(UserProfile userProfile, Long ratingId);
 }
