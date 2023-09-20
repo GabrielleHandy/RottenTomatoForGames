cf845905562e901c732ebb2e14d0a0177ac18ecf
GabrielleHandy <gehandy123@gmail.com>
2023-09-20 01:31:04 -0400

    Autowire GameService in GameController

--- src/main/java/com/example/rottentomatoforgames/controller/GameController.java
+++ src/main/java/com/example/rottentomatoforgames/controller/GameController.java
@@ -1,9 +1,17 @@
 package com.example.rottentomatoforgames.controller;
 
+import com.example.rottentomatoforgames.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/games")
 public class GameController {
+
+    private GameService gameService;
+    @Autowired
+    public void setGameService(GameService gameService) {
+        this.gameService = gameService;
+    }
 }
